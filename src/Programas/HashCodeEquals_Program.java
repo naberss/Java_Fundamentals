b48ced3a943fae85dadb4cce7a39222f621a2396
Lucas Moreira <lucas-berto@hotmail.com>
2021-06-05 23:41:21 -0300

    [#22 -> HashCode  & Equals] - Incremento
    *Implementação dos métodos (HashCode  & Equals) em classes customizadas comparações utilizando ambos em conjunto.

---
+++ src/Programas/HashCodeEquals_Program.java
@@ -0,0 +1,34 @@
+package Programas;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import classes.HashCodeEquals_TestClass;
+
+public class HashCodeEquals_Program {
+
+	public static void main(String[] args) {
+
+		List<HashCodeEquals_TestClass> list1 = Arrays.asList(
+				new HashCodeEquals_TestClass("1", "lucas-berto@hotmail.com"),
+				new HashCodeEquals_TestClass("1", "lucas-berto@hotmail.com"));
+
+		List<HashCodeEquals_TestClass> list2 = Arrays.asList(
+				new HashCodeEquals_TestClass("1", "sandrabberto@gmail.com"),
+				new HashCodeEquals_TestClass("2", "sandrabberto@gmail.com"));
+
+		System.out.println(list1.get(1).hashCode());
+		System.out.println(list2.get(1).hashCode());
+
+		if (list1.get(0).hashCode() == list1.get(1).hashCode()) {
+			if (list1.get(0).equals(list1.get(1))) {
+				System.out.println("pera");
+			} else {
+				System.out.println("mamao");
+			}
+		}
+
+	}
+
+}
